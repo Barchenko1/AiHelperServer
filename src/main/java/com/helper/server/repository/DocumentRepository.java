ebc15114264bead07f6ff02c93a99b2ad42caf74
pavel <wyerer123@gmail.com>
2025-09-30 12:41:07 +0200

    delete rag

--- src/main/java/com/helper/server/repository/DocumentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.helper.server.repository;
-
-import com.helper.server.modal.LoadedDocument;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentRepository extends JpaRepository<LoadedDocument, Long> {
-    boolean existsByFilenameAndContentHash(String filename, String contentHash);
-}
